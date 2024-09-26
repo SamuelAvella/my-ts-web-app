f7c8e7f4060d1944aaa151660e7ffdac96083c7d
Samuel Avella <saveper1907@g.educaand.es>
2024-09-26 14:18:11 +0200

    Add Promise

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,41 @@
-const cards = document.createElement("card-container")
-const btn = document.getElementById("btn")
-const inputName = document.getElementById("input-name") as HTMLInputElement
+window.addEventListener('DOMContentLoaded',() => {
+        const cards = document.createElement("card-container") as HTMLElement
+        const btn = document.getElementById("btn") as HTMLInputElement
+        const inputName = document.getElementById("input-name") as HTMLInputElement
+        const myAlert = document.getElementById("myAlert") as HTMLElement
 
-function createCard(text:String){
+        btn.addEventListener('click', async () => {
 
-}
+            const myPromise = new Promise((resolve, reject) =>{
+                setTimeout(() => {
+                    if (inputName && inputName.value.trim() !== "") {
+                      resolve(inputName.value);
+                    } else {
+                      reject("Hubo un error");
+                    }
+                }, 2000);
+            })
 
-if(btn !== null && inputName !== null){
-    btn.addEventListener("click", () => createCard(inputName.value))    
-}
+            myPromise.then(() => {
+                    const ionCard = document.createElement("ion-card")
+                    const ionCardContent = document.createElement("ion-card-content")
+
+                    ionCardContent.textContent = inputName.value;
+    
+                    ionCard.appendChild(ionCardContent)
+                    cards.appendChild(ionCard)
+                
+                    inputName.value = ''
+              })
+              .catch(async error => {
+                if(myAlert){
+                    await (myAlert as any).present()
+                }
+              });    
+        })
+    }
+
+)
+
+
 
